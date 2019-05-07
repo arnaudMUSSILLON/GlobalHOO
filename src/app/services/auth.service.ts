938f45aa25bb60522feeaaa0fdc6cabf28259553
arnaudMUSSILLON <arnaud.mussillon@orange.fr>
2019-05-07 21:32:02 +0200

    update profile feature

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tokenNotExpired } from 'angular2-jwt';
 import { environment } from '../../environments/environment';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,16 +15,42 @@ export class AuthService {
 
   constructor(private http: HttpClient, private storage: NativeStorage) { }
 
+  /**
+   * Call to the API to try to register the user
+   * @param user 
+   */
   registerUser(user) {
     let headers = new HttpHeaders({'Content-Type': 'application/json'});
     return this.http.post(environment.apiUrl+'/user/create', user, {headers:headers});
   }
 
+  /**
+   * Ask the API if the credentials are correct
+   * @param user 
+   */
   authenticateUser(user) {
     let headers = new HttpHeaders({'Content-Type': 'application/json'});
     return this.http.post(environment.apiUrl+'/user/authenticate', user, {headers:headers});
   }
 
+  /**
+   * Try to reach the API to request a change of informations
+   * @param user
+   */
+  updateUser(user) {
+    let headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return Observable.fromPromise(this.getToken())
+      .flatMap(token => {
+        headers.append('Authorization', token);
+        return this.http.post(environment.apiUrl+'/user/update', user, {headers:headers});
+    });
+  }
+
+  /**
+   * Store the token and the user in the storage of the device
+   * @param token 
+   * @param user 
+   */
   storeUserData(token, user) {
     this.authToken = token;
     this.user = user;
@@ -31,13 +58,34 @@ export class AuthService {
     this.storage.setItem('user', this.user);
   }
 
-  loadToken() {
-    this.storage.getItem('id_token')
-      .then(data => this.authToken = data);
-  }
-
+  /**
+   * Check if the token expired
+   */
   loggedIn() {
     return tokenNotExpired('id_token', this.authToken);
   }
 
+  /**
+   * Return the token from the storage
+   */
+  getToken() {
+    return this.storage.getItem('id_token')
+      .then(data => {
+        return data.token;
+      });
+  }
+
+  /**
+   * Return the user from the storage
+   */
+  getUser(): any {
+    return this.storage.getItem('user')
+      .then(data => {
+        return {
+          first_name: data.first_name,
+          last_name: data.last_name,
+          email: data.email
+        };
+      });
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,86 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  user: any;
+
+  onlyLettersRegex = /^[A-Z]+$/i;
+  updateform;
+
+  validation_messages = {
+    'first_name': [
+      { type: 'required', message: 'First name is required.' },
+      { type: 'maxlength', message: 'Cannot exceed 15 characters' },
+      { type: 'pattern', message: 'Must contain only letters'}
+    ],
+    'last_name': [
+      { type: 'required', message: 'Last name is required.' },
+      { type: 'maxlength', message: 'Cannot exceed 20 characters' },
+      { type: 'pattern', message: 'Must contain only letters'}
+    ],
+    'email': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'maxlength', message: 'Cannot exceed 50 characters' },
+      { type: 'email', message: 'Your email must be valid.' },     
+    ]
+  }
+
+  constructor(private authService: AuthService, private toast: ToastController) {
+    this.authService.getUser().then((data) => {
+      this.user = data;
+      this.updateform = new FormGroup({
+        first_name: new FormControl(this.user.first_name, Validators.compose([
+          Validators.required,
+          Validators.maxLength(15),
+          Validators.pattern(this.onlyLettersRegex)
+        ])),
+        last_name: new FormControl(this.user.last_name, Validators.compose([
+          Validators.required,
+          Validators.maxLength(20),
+          Validators.pattern(this.onlyLettersRegex)
+        ])),
+        email: new FormControl({value: this.user.email, disabled: true}, Validators.compose([
+          Validators.required,
+          Validators.maxLength(50),
+          Validators.email
+        ]))
+      });
+    });
+  }
+
+  update() {
+    this.authService.getUser().then(storedUser => {
+      let user = {
+        first_name: this.updateform.get('first_name').value,
+        last_name: this.updateform.get('last_name').value,
+        email: this.updateform.get('email').value,
+        current_email: storedUser.email
+      };
+      this.authService.updateUser(user).subscribe((data:any) => {
+        if(data.success) {
+          this.authService.storeUserData(data.token, data.user);
+          this.presentSuccessToast(data.msg);
+        } else {
+          this.presentDangerToast(data.msg);
+        }
+      });
+    })
+  }
+
+  async presentDangerToast(msg: string) {
+    let t = await this.toast.create({ message : msg, duration: 3000, color: 'danger' });
+    t.present();
+  }
+
+  async presentSuccessToast(msg: string) {
+    let t = await this.toast.create({ message : msg, duration: 3000, color: 'success' });
+    t.present();
+  }
+}
